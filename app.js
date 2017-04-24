fd45652072e709a0e1908d68d6563394bbae089a
Manuel Rúa <imanurua@gmail.com>
2017-04-24 18:02:20 +0200

    Definición del primer modelo Product y ajuste del método POST

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
+const Product = require('./models/product')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 // BD Setup
 mongoose.connect("mongodb://mongo:27017/shop", (err, res) => {
-  if (err) throw err
+  if (err) {
+      return console.log(`Error al conectar a la base de datos: ${err}`)
+  }
   console.log('Conexión a la base de datos establecida')
 
   app.listen(port, () => {
@@ -29,8 +33,21 @@ app.get('/api/product/:productId', (req, res) => {
 })
 
 app.post('/api/product', (req, res) => {
+  console.log('POST /api/product')
   console.log(req.body)
-  res.status(200).send({message: 'El producto se ha recibido'})
+
+  let product = new Product()
+  product.name = req.body.name
+  product.picture = req.body.picture
+  product.price = req.body.price
+  product.category = req.body.category
+  product.description = req.body.description
+
+  product.save((err, productStored) => {
+    if (err) res.status(500).send({message: `Error al salvar en la base de datos: ${err}`})
+
+    res.status(200).send({product: productStored})
+  })
 })
 
 app.put('/api/product/:productId', (req, res) => {

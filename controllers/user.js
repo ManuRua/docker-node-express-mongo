1ce948b44feafb32496903fbe7a2ede394a6f358
Manuel Rúa <imanurua@gmail.com>
2017-04-27 00:01:46 +0200

    Login y registro de usuarios

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 const User = require('../models/user')
-const service = require('../services')
+const services = require('../services')
 
 function signUp (req, res) {
   const user = new User({
@@ -13,12 +13,21 @@ function signUp (req, res) {
   user.save((err) => {
     if (err) res.status(500).send({ message: `Error al crear el usuario: ${err}`})
 
-    return res.status(200).send({ token: service.createToken(user) })
+    return res.status(200).send({ token: services.createToken(user) })
   })
 }
 
 function signIn (req, res) {
+  User.find({ email: req.body.email }, (err, user) => {
+    if (err) return res.status(500).send({ message: err })
+    if (!user) return res.status(404).send({ message: 'No existe el usuario' })
 
+    req.user = user
+    res.status(200).send({
+      message: 'Te has logueado correctamente',
+      token: services.createToken(user)
+    })
+  })
 }
 
 module.exports = {
